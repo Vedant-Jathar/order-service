988b48206f1bce48362d9f60c2af9b330c63d4eb
Vedant_Jathar <jatharvedant16@gmail.com>
2025-08-09 21:59:55 +0530

    uncomment connection with kafka

--- server.ts
+++ server.ts
@@ -2,24 +2,24 @@ import app from "./src/app";
 import config from "config";
 import logger from "./src/config/logger";
 import connectDB from "./src/config/db";
-// import { MessageBroker } from "./src/types/broker";
-// import { createMessageBroker } from "./src/common/middleware/fatories/brokerFactory";
+import { MessageBroker } from "./src/types/broker";
+import { createMessageBroker } from "./src/common/middleware/fatories/brokerFactory";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
 
-  // let messagebroker: MessageBroker = null
+  let messagebroker: MessageBroker = null
 
   try {
     await connectDB();
 
     // Creating the consumer and reading the messages from the broker:
-    // messagebroker = createMessageBroker()
-    // await messagebroker.connectProducer()
-    // logger.info("Connected to the producer")
-    // await messagebroker.connectConsumer()
-    // logger.info("Connected to the consumer")
-    // await messagebroker.consumeMessage(["product", "topping"], false)
+    messagebroker = createMessageBroker()
+    await messagebroker.connectProducer()
+    logger.info("Connected to the producer")
+    await messagebroker.connectConsumer()
+    logger.info("Connected to the consumer")
+    await messagebroker.consumeMessage(["product", "topping"], false)
 
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
@@ -28,9 +28,9 @@ const startServer = async () => {
         process.exit(1);
       });
   } catch (err) {
-    // if (messagebroker) {
-    //   await messagebroker.disconnectConsumer()
-    // }
+    if (messagebroker) {
+      await messagebroker.disconnectConsumer()
+    }
     logger.error("Error happened: ", err.message);
     process.exit(1);
   }
